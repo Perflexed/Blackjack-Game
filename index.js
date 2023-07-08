7d5691c14c0dad010ab1ecbe41292049ad09e2fe
Josh1Flint <54628530+Josh1Flint@users.noreply.github.com>
2023-07-09 00:29:44 +0100

    Cards Element and Array

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 //CARDS
-let firstCard=20
-let secondCard=0
-let thirdCard=0
-let forthCard=0
-let fifthCard=0
-let sixthCard=0
-let seventhCard=0
+let firstCard=8
+let secondCard=10
+
+let cards=[firstCard,secondCard]
+console.log(cards)
 //CONDITIONS
 let hasBlackjack=false //this is a boolean statement (true or false)
 let isAlive=true
+//INPUT
+cards.push(6)
+console.log(cards)
 //OUTPUT
 let message=""
 let messageEl=document.getElementById("message-el")
+let cardsEl=document.getElementById("cards-el")
 let sum=firstCard+secondCard
 let sumEl=document.getElementById("sum-el")
 //START GAME
 function startGame() {
+    renderGame()
+}
+function renderGame() {
+    cardsEl.textContent="Cards:"+cards[0]+" "+cards[1]+" "+cards[2] //arrays are 0 indexed, meaning that they start from 0 rather than 1.
     sumEl.textContent="Sum: "+sum
     if (sum<=20) { //conditions work kinda like booleans, if the condition is met (if the statement is true) then the code is run, if not (false) then the code is not run.
         message="Draw a new card?"
@@ -28,3 +34,8 @@ function startGame() {
     }console.log(message)
     messageEl.textContent=message
     }
+function newCard() {
+    let card=1
+    sum+=card
+    renderGame()
+}
