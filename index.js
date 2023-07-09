77a6c23c0b0c871eee8719789aef43b748f5dba1
Josh1Flint <54628530+Josh1Flint@users.noreply.github.com>
2023-07-09 16:01:01 +0100

    Made Cards Random

--- index.js
+++ index.js
@@ -1,27 +1,42 @@
-//CARDS
-let firstCard=8
-let secondCard=10
-
-let cards=[firstCard,secondCard]
-console.log(cards)
-//CONDITIONS
-let hasBlackjack=false //this is a boolean statement (true or false)
-let isAlive=true
-//INPUT
-cards.push(6)
-console.log(cards)
-//OUTPUT
-let message=""
+//PLAYER
+let player={
+    name:"Josh",
+    cash:200,
+}
+let playerEl=document.getElementById("player-el")
+playerEl.textContent=player.name+": $"+player.cash
+//CASH
+//function cashDeduct() {
+//    player.cash=10
+//    console.log("cash taken!")
+//    return player.cash
+//}
+//ELEMENTS
 let messageEl=document.getElementById("message-el")
 let cardsEl=document.getElementById("cards-el")
-let sum=firstCard+secondCard
 let sumEl=document.getElementById("sum-el")
+//CONDITIONS
+let hasBlackjack=false //this is a boolean statement (true or false)
+let isAlive=false
+//OUTPUT
+let message=""
+let cards=[]
+let sum=" "
 //START GAME
 function startGame() {
+    isAlive=true
+    let firstCard=getRandomCard()
+    let secondCard=getRandomCard()
+    cards=[firstCard,secondCard]
+    sum=firstCard+secondCard
     renderGame()
 }
+//RENDER GAME
 function renderGame() {
-    cardsEl.textContent="Cards:"+cards[0]+" "+cards[1]+" "+cards[2] //arrays are 0 indexed, meaning that they start from 0 rather than 1.
+    cardsEl.textContent="Cards: "
+    for(let i=0; i<cards.length; i++) {
+        cardsEl.textContent+=cards[i]+" " //arrays are 0 indexed, meaning that they start from 0 rather than 1.
+    }
     sumEl.textContent="Sum: "+sum
     if (sum<=20) { //conditions work kinda like booleans, if the condition is met (if the statement is true) then the code is run, if not (false) then the code is not run.
         message="Draw a new card?"
@@ -34,8 +49,25 @@ function renderGame() {
     }console.log(message)
     messageEl.textContent=message
     }
+//NEW CARD
 function newCard() {
-    let card=1
-    sum+=card
-    renderGame()
+    if(isAlive==true&&hasBlackjack===false) { //THIS IS AN "AND" STATEMENT AND REQUIRES BOTH CONDITIONS ARE MET TO CONTINUE.
+        let card=getRandomCard()
+        sum+=card
+        cards.push(card)
+        renderGame()
+    }
+}
+//RANDOM CARD
+function getRandomCard() {
+    let randomCard=Math.floor(Math.random()*13)+1
+    //THIS IS JUST A PLACEHOLDER BECAUSE MAKING THE ACE HAVE BOTH 1 OR 11 OPTIONS IS TOO COMPLEX RN.
+    if (randomCard===1){
+        return 11
+    }else if (randomCard>11) {
+        return 10
+    }else {
+        return (randomCard)
+    }
+    //END OF PLACEHOLDER.
 }
